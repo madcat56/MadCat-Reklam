64147cf350efed71d537aa1b2ebaf6b69f87a10e
madcat56 <80549436+madcat56@users.noreply.github.com>
2021-04-24 17:55:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,10 +36,10 @@ if(message.channel.type === 'dm') {
 
 if (message.content.startsWith("https://discord.gg/")) {
     
-  message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/EXCuvNd`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
+  message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/QbvDN7s7wr`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
     
   client.channels.get("755636700482633791").send(
-`> <:ARBELE_UPUP:791411050238836766> By: <@${message.author.id}> ✅ 
+`> <:By: <@${message.author.id}> ✅ 
 ${message.content}`)
 }
     }
@@ -51,10 +51,10 @@ ${message.content}`)
 client.on('ready', () => {
 console.log(`ON ${client.guilds.size} Servers`);
 console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`ڕیکلامەکە بنێرە بۆمن دایدەنێم☑️`) 
+client.user.setGame(`MAD CAT UP BY JUST STAF`) 
 });
 
 
 
 
-client.login("");//تۆکین لێرە دانێ
+client.login("ODMzMTE0MTg3Mzc4NzIwODQ4.YHtoDA.FSUvNQzBhNMDKeiUrJ6rVgqvcN4");//تۆکین لێرە دانێ
